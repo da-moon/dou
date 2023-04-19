3ae6cd6dd49a464caf00b24195b796f335d337dc
da-moon <contact@havi.dev>
2023-04-19 18:31:01 -0400

    Squashed 'github.com/deploy-adop/' content from commit c993ed3
    git-subtree-dir: github.com/deploy-adop
    git-subtree-split: c993ed3c37021ed82a207e240d7e92d72dc9eb42

---
+++ params.sh
@@ -0,0 +1,11 @@
+export AWS_ACCESS_KEY_ID=
+export AWS_ACCESS_SECRET_KEY=
+export AWS_DEFAULT_REGION=us-east-1
+export AWS_AMI=ami-841f46ff
+export AWS_INSTANCE_TYPE=m4.2xlarge
+export AWS_VPC_ID=vpc-62996e04
+export AWS_SPOT_PRICE=0.2
+export ADOP_DIR=~/code/deployed-adop-docker-compose
+export ADOP_USER=uadop
+export ADOP_PWD=padop123
+export ADOP_COMMIT=d32cfa98ac9574a7c400fd37e927b52adc6fc7b9
