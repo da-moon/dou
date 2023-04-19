41c03c27f72d02e474cb41fc8e22f7ffefa02cbf
da-moon <contact@havi.dev>
2023-04-19 18:54:22 -0400

    Squashed 'github.com/terraform-aws-ecs-alb-with-single-target-group-spw/' content from commit d8ef57f
    git-subtree-dir: github.com/terraform-aws-ecs-alb-with-single-target-group-spw
    git-subtree-split: d8ef57fb7951dc5be04243fc92a393b5601f927d

---
+++ main.tf
@@ -0,0 +1,88 @@
+# ALB
+resource "aws_alb" "alb" {
+  name_prefix     = var.run_env
+  internal        = true
+  security_groups = [var.security_groups]
+  subnets         = split(",", var.subnets)
+  idle_timeout    = var.alb_idle_timeout
+
+  tags = {
+    env = var.run_env
+    ## Adding servicename for Datadog filters to match other filters using "servicename" for other legacy apis.
+    servicename = var.service_name
+    servicetype = var.optional_subdomain
+  }
+}
+
+resource "aws_alb_target_group" "alb_target_group" {
+  #name                 = "${var.run_env}-${var.service_name}-${var.optional_subdomain}"
+  port                 = var.alb_target_group_port
+  protocol             = var.target_group_protocol
+  vpc_id               = var.vpc_id
+  deregistration_delay = var.target_group_drain_duration
+
+  health_check {
+    healthy_threshold   = var.health_check_healthy_threshold
+    unhealthy_threshold = var.health_check_unhealthy_threshold
+    timeout             = var.health_check_timeout
+    protocol            = var.health_check_protocol
+    path                = var.health_check_path
+    interval            = var.health_check_interval
+    matcher             = var.health_check_matcher
+  }
+
+  depends_on = [aws_alb.alb]
+}
+
+resource "aws_alb_listener" "alb_listener" {
+  load_balancer_arn = aws_alb.alb.arn
+  port              = var.alb_listener_port
+
+  default_action {
+    target_group_arn = aws_alb_target_group.alb_target_group.arn
+    type             = "forward"
+  }
+
+  depends_on = [aws_alb_target_group.alb_target_group]
+}
+
+resource "aws_alb_listener_rule" "alb_listener_rule" {
+  listener_arn = aws_alb_listener.alb_listener.arn
+  priority     = 1
+
+  action {
+    target_group_arn = aws_alb_target_group.alb_target_group.arn
+    type             = "forward"
+  }
+
+  condition {
+    field  = "path-pattern"
+    values = ["*"]
+  }
+
+  depends_on = [aws_alb_listener.alb_listener]
+}
+
+##########################
+#        DNS             #
+##########################
+
+resource "aws_route53_record" "dns_entry" {
+  zone_id = var.hosted_zone_id
+  name    = "${var.service_name}.${var.optional_subdomain}.${data.aws_route53_zone.hosted_zone.name}"
+  type    = "A"
+
+  alias {
+    name                   = aws_alb.alb.dns_name
+    zone_id                = aws_alb.alb.zone_id
+    evaluate_target_health = false
+  }
+
+  depends_on = [aws_alb_listener_rule.alb_listener_rule]
+}
+
+data "aws_route53_zone" "hosted_zone" {
+  zone_id      = var.hosted_zone_id
+  private_zone = true
+}
+
